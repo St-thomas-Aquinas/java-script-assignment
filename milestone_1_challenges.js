7eca777969175e914b8dc69b63bc7037df9dd231
St-thomas-Aquinas <maxkuria254@gmail.com>
2025-05-20 16:41:28 +0300

    Add challenge one ,two, three

---
+++ milestone_1_challenges.js
@@ -0,0 +1,31 @@
+//Challenge 1 : Sum of two numbers
+function addition(num1,num2){
+
+    return num1 + num2
+
+}
+console.log("Challenge 1 : Sum of two numbers is")
+  let result = addition(100,100)
+  console.log(result)
+// End
+
+
+//challenge 2 : converting 10 min into seconds.
+function convert(){
+    let minutes = 10;
+    let results = minutes*60;
+    console.log("Iam the function that is converting minnutes into seconds")
+    console.log(results)
+}
+convert()
+//end
+
+//Challenge 3 : Calculating the perimenter of a rectangle
+function perimenter(){
+    let length = 20;
+    let width = 10;
+    let answer =  ((length + width)*2)
+    console.log("Iam the fuction for calculating perimeter of a rectangle")
+    console.log(answer)
+}
+perimenter()
